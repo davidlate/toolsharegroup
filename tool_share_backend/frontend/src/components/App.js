813086cb52b8a5ca7846ef896ab91ab1572210ce
David Earley <david.earley2@gmail.com>
2023-03-22 15:54:01 -0400

    Added and routed the home, join, and create pages

--- tool_share_backend/frontend/src/components/App.js
+++ tool_share_backend/frontend/src/components/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+
     }
 
 
     render(){
-        return <h1>Testing React Code</h1>
+        return (<div>
+        <HomePage />
+        </div>)
+
     }
 }
 
 const appDiv = document.querySelector('#app');
-render(<App />, appDiv);
+
+render(<App name='tim' />, appDiv);
